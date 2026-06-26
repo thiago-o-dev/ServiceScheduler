bf6f1ee3f163f51969bb5b30746b8a5715910ef2
Thiago H <thiagodiacho@gmail.com>
2026-06-26 20:58:17 -0300

    Creates simple admin model with less than advised security

---
+++ ServiceScheduler.Backend/Services/ServiceScheduler/ServiceScheduler.Domain/Entities/Admin.cs
@@ -0,0 +1,41 @@
+﻿using ServiceScheduler.Domain.Policies;
+using SharedKernel.Abstractions;
+using SharedKernel.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceScheduler.Domain.Entities;
+
+public class Admin : LifeCycleEntity
+{
+    // n vo botar mais nada, vou deixar validação na camada de aplicação
+    public string Name { get; private set; } = string.Empty;
+    public string Phone { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+
+    private Admin() { }
+
+    public static Admin Create(string name, string phone, string email)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainValidationException("Nome é obrigatório.");
+        if (string.IsNullOrWhiteSpace(phone))
+            throw new DomainValidationException("Celular é obrigatório.");
+        if (string.IsNullOrWhiteSpace(email))
+            throw new DomainValidationException("Email é obrigatório.");
+
+        var admin = new Admin
+        {
+            Name = name.Trim(),
+            Phone = phone.Trim(),
+            Email = email.Trim(),
+        };
+
+        AdminPolicy.EnsureCanCreateAdmin(admin);
+
+        // TODO: estourar evento pro mailhog pra mostrar q foi criado um novo usuário
+
+        return admin;
+    }
+}
---
+++ ServiceScheduler.Backend/Services/ServiceScheduler/ServiceScheduler.Domain/Policies/AdminPolicy.cs
@@ -0,0 +1,25 @@
+﻿using ServiceScheduler.Domain.Entities;
+using SharedKernel.Exceptions;
+using System.Net.Mail;
+
+namespace ServiceScheduler.Domain.Policies;
+
+public static class AdminPolicy
+{
+    public static void EnsureCanCreateAdmin(Admin admin)
+    {
+        if (admin.Name.Length < 2)
+        {
+            throw new DomainValidationException("Nome deve ser maior que duas letras");
+        }
+
+        try
+        {
+            _ = new MailAddress(admin.Email);
+        }
+        catch
+        {
+            throw new DomainValidationException("O e-mail informado é inválido.");
+        }
+    }
+}
