dc3c33f4125c1571ee74595e2e5260b0be5ce232
Thiago H <thiagodiacho@gmail.com>
2026-06-26 22:12:50 -0300

    Creates basic empty API Controllers

---
+++ ServiceScheduler.Backend/Services/ServiceScheduler/ServiceScheduler.Api/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceScheduler.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class AdminController : ControllerBase
+{
+}
