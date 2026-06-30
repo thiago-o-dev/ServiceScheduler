499c915eef34f200e2fd0b8056d353218db9bd1a
Thiago H <thiagodiacho@gmail.com>
2026-06-30 04:19:55 -0300

    Includes discount amount in bundle dto

--- ServiceScheduler.Backend/Services/ServiceScheduler/ServiceScheduler.Application/Features/ServiceBundles/ServiceBundleDto.cs
+++ ServiceScheduler.Backend/Services/ServiceScheduler/ServiceScheduler.Application/Features/ServiceBundles/ServiceBundleDto.cs
@@ -1,3 +1,3 @@
 namespace ServiceScheduler.Application.Features.ServiceBundles;
 
-public record ServiceBundleDto(Guid Id, string Name, string Description, IReadOnlyList<Guid> ServiceIds, decimal Value);
+public record ServiceBundleDto(Guid Id, string Name, string Description, IReadOnlyList<Guid> ServiceIds, decimal Value, decimal Discount);
--- ServiceScheduler.Backend/Services/ServiceScheduler/ServiceScheduler.Application/Features/ServiceBundles/ServiceBundleQueries.cs
+++ ServiceScheduler.Backend/Services/ServiceScheduler/ServiceScheduler.Application/Features/ServiceBundles/ServiceBundleQueries.cs
@@ -1,12 +1,13 @@
 using ServiceScheduler.Application.Abstractions;
 using SharedKernel.Abstractions.CQRS;
 using SharedKernel.Exceptions;
+using System.Reflection.Metadata;
 
 namespace ServiceScheduler.Application.Features.ServiceBundles;
 
 public sealed record GetServiceBundleByIdQuery(Guid Id) : IQueryRequest<ServiceBundleDto>;
 
-public sealed class GetServiceBundleByIdQueryHandler(IServiceBundleRepository bundleRepository)
+public sealed class GetServiceBundleByIdQueryHandler(IServiceBundleRepository bundleRepository, IServiceRepository serviceRepository)
     : IRequestHandler<GetServiceBundleByIdQuery, ServiceBundleDto>
 {
     public async Task<ServiceBundleDto> HandleAsync(GetServiceBundleByIdQuery query, CancellationToken cancellationToken = default)
@@ -14,19 +15,25 @@ public sealed class GetServiceBundleByIdQueryHandler(IServiceBundleRepository bu
         var bundle = await bundleRepository.GetByIdAsync(query.Id, cancellationToken)
             ?? throw new NotFoundException($"Pacote com ID '{query.Id}' não encontrado.");
 
-        return new ServiceBundleDto(bundle.Id, bundle.Name, bundle.Description, bundle.ServiceIds.ToList(), bundle.Value);
+        var services = await serviceRepository.GetAllAsync();
+
+        var servicesGrossValue = services.Where(s => bundle.ServiceIds.Contains(s.Id)).Select(s => s.Value).Sum();
+
+        return new ServiceBundleDto(bundle.Id, bundle.Name, bundle.Description, bundle.ServiceIds.ToList(), bundle.Value, (1 - bundle.Value / servicesGrossValue)*100);
     }
 }
 
 public sealed record ListServiceBundlesQuery : IQueryRequest<IReadOnlyList<ServiceBundleDto>>;
 
-public sealed class ListServiceBundlesQueryHandler(IServiceBundleRepository bundleRepository)
+public sealed class ListServiceBundlesQueryHandler(IServiceBundleRepository bundleRepository, IServiceRepository serviceRepository)
     : IRequestHandler<ListServiceBundlesQuery, IReadOnlyList<ServiceBundleDto>>
 {
     public async Task<IReadOnlyList<ServiceBundleDto>> HandleAsync(ListServiceBundlesQuery query, CancellationToken cancellationToken = default)
     {
         var bundles = await bundleRepository.GetAllAsync(cancellationToken);
 
-        return bundles.Select(b => new ServiceBundleDto(b.Id, b.Name, b.Description, b.ServiceIds.ToList(), b.Value)).ToList();
+        var services = await serviceRepository.GetAllAsync();
+
+        return bundles.Select(b => new ServiceBundleDto(b.Id, b.Name, b.Description, b.ServiceIds.ToList(), b.Value, (1 - b.Value / services.Where(s => b.ServiceIds.Contains(s.Id)).Select(s => s.Value).Sum()) * 100)).ToList();
     }
 }
